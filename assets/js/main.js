acd26a35660e319f9a4fcf5ce378b34110961ecd
Thilina342 <thilinabandar67@gmail.com>
2022-03-27 17:08:57 +0530

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(window).on('load', function () {
+     $("#loader_container").fadeOut(6000);
+ });
+ $(document).ready(function () {
+     console.log("Document Ready");
+ });
+
+ $(function () {
+     console.log("Document Loaded");
+ });
